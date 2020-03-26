e6720b43157dd74d9b9855641fe6a66f07331aac
root <root@storage4.citec.com>
2020-03-26 13:21:45 +0900

    pulling for vdbench50407

--- vdb.sh
+++ vdb.sh
@@ -1,2 +1,3 @@
 VDBPATH=/root/yoon/vdbench
-/root/yoon/vdbench/vdbench -f /root/yoon/git/vdbench/test7.par -o /nas/yoon/test7
+#/root/yoon/vdbench/vdbench -f /root/yoon/git/vdbench/test7.par -o /nas/yoon/test7
+/root/yoon/vdbench/vdbench -f /root/yoon/git/vdbench/test8.par -o /nas/yoon/test8
